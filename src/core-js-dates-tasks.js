c06c8ada67973c37fc078ad21a1a20c867c004be
Sergey <seleznev-krista@mail.ru>
2024-03-24 20:10:53 +0300

    fix

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -192,11 +192,12 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(date) {
-  const start = new Date(date.getUTCFullYear(), 0);
-  const days = (date - start) / 1000 / 60 / 60 / 24 + 1;
-  const d = start.getUTCDay();
-  return Math.trunc((days + d - 1) / 7) + 1;
+function getWeekNumberByDate(/* date */) {
+  // const start = new Date(date.getUTCFullYear(), 0);
+  // const days = (date - start) / 1000 / 60 / 60 / 24 + 1;
+  // const d = start.getUTCDay();
+  // return Math.trunc((days + d - 1) / 7) + 1;
+  throw new Error('Not implemented');
 }
 
 /**
